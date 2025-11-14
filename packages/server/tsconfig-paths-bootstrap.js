313c252d0fe7afda719e13ca1a296279e879938c
Cellestialstorm <rohankhatiwara76@gmail.com>
2025-11-14 13:58:51 +0530

    fix(server): correct tsconfig paths registration and start script
    - Update start script to require tsconfig-paths/register before bootstrap
    - Use path.join instead of string concatenation for baseUrl in tsconfig-paths-bootstrap.js
    - Add missing comma after paths option in tsconfig-paths registration
    - Import path module in tsconfig-paths-bootstrap.js for proper path joining

--- packages/server/tsconfig-paths-bootstrap.js
+++ packages/server/tsconfig-paths-bootstrap.js
@@ -1,7 +1,8 @@
 const tsConfigPaths = require("tsconfig-paths");
 const tsConfig = require("./tsconfig.json");
+const path = require("path");
 
 tsConfigPaths.register({
-  baseUrl: __dirname + "/dist",
-  paths: tsConfig.compilerOptions.paths
+  baseUrl: path.join(__dirname, "dist"),
+  paths: tsConfig.compilerOptions.paths,
 });
